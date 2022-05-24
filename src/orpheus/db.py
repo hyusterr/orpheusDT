cbc6edd4c3fb915eb48b150ecb53215dade26204
廖政華 <45114413+kamelliao@users.noreply.github.com>
2022-05-24 15:44:43 +0800

    require specify database name when creating db connection

--- src/orpheus/db.py
+++ src/orpheus/db.py
@@ -3,12 +3,20 @@ from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
 class DatabaseManager:
-    def __init__(self):
+    def __init__(self, dbname: str):
+        """
+        Parameters
+        ----------
+        db_name : Specify the working database.
+        """
         self.client = self._connect_db()
 
-        self.database = None
-        self.dataset = None
-        self.model = None
+        if dbname not in self.client.list_database_names():
+            click.secho(f'Note: Database {dbname} not exists. Creating a new one ...', fg='yellow')
+        
+        self.database = self.client[dbname]
+        self.dataset = self.database["dataset"]
+        self.model = self.database["model"]
     
     def _connect_db(self):
         """
@@ -28,23 +36,11 @@ class DatabaseManager:
             click.secho("Cannot connect to the database.", fb='red')
         
         return client
-    
-    def use_database(self, db_name: str):
-        """
-        Parameters
-        ----------
-        db_name : Name of database.
-        """
-        if db_name not in self.client.list_database_names():
-            click.secho(f'Note: Database {db_name} not exists. Creating a new one ...', fg='yellow')
-        
-        self.database = self.client[db_name]
-        self.dataset = self.database["dataset"]
-        self.model = self.database["model"]
 
     def drop_database(self):
         """
-        Drop current database (database) from the MongoDB client.
+        Drop current database from the MongoDB client.
+        TODO: This is a dangerous operation and thus should require authentication.
         """
         if self.database is not None:
             self.client.drop_database(self.database)
