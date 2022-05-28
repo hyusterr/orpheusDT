307eea98e80ac2dd62d6778a497bb9a1b5965c8f
黃禹翔 <chu1231104@gmail.com>
2022-05-28 20:29:34 +0800

    add test interface

---
+++ src/orpheus/test.py
@@ -0,0 +1,22 @@
+#!usr/bin/python3
+# encoding: utf-8
+
+import pandas as pd
+from orpheus import Orpheus
+from sklearn.tree import DecisionTreeClassifier
+
+data_name = '../../data/WFH_WFO_dataset.csv'
+target = 'Target'
+
+df = pd.read_csv(data_name)
+features = list(df.columns); features.remove(target)
+X, y = df[features], df[target]
+
+# usage for our class
+orpheus_instance = Orpheus(
+    DecisionTreeClassifier(random_state=0),
+    X,
+    y,
+)
+orpheus_instance.train()
+orpheus_instance.save_metaData_toDB()
