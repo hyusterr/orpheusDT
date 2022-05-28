b5bb2b08bc28c4ef6dbc508a32102816fec5d060
廖政華 <45114413+kamelliao@users.noreply.github.com>
2022-05-28 15:06:14 +0800

    add user management functions

--- src/orpheus/db.py
+++ src/orpheus/db.py
@@ -1,24 +1,34 @@
 import click
+from typing import List
 from pymongo import MongoClient
+from pymongo.database import Database
 from pymongo.errors import ConnectionFailure
 
+
 class DatabaseManager:
-    def __init__(self, dbname: str):
+    def __init__(
+        self,
+        dbname: str,
+        username: str = None,
+        password: str = None
+    ):
         """
         Parameters
         ----------
-        db_name : Specify the working database.
+        dbname : Specify the working database.
         """
-        self.client = self._connect_db()
+        self.name = dbname
+        self.client = self._connect_db(username, password)
 
+        # Create database
         if dbname not in self.client.list_database_names():
             click.secho(f'Note: Database {dbname} not exists. Creating a new one ...', fg='yellow')
-        
-        self.database = self.client[dbname]
-        self.dataset = self.database["dataset"]
-        self.model = self.database["model"]
+
+        self.db = Database(self.client, name=dbname)
+        self.data = self.db.get_collection('data')
+        self.model = self.db.get_collection('model')
     
-    def _connect_db(self):
+    def _connect_db(self, username: str = None, password: str = None):
         """
         Connect to MongoDB server.
 
@@ -26,8 +36,13 @@ class DatabaseManager:
         ----------
         client : Client for a MongoDB instance.
         """
-        # TODO: should use username and password
-        client = MongoClient(host='localhost', port=27017)
+        client = MongoClient(
+            host='localhost',
+            port=27017,
+            username=username,
+            password=password,
+            authSource=self.name
+        )
 
         # Check connection
         try:
@@ -37,13 +52,21 @@ class DatabaseManager:
         
         return client
 
+    # user management
+    def create_user(self, user: str, pwd: str, roles: List[str]):
+        self.db.command('createUser', user, pwd=pwd, roles=roles)
+
+    def grant_roles(self, user: str, roles: List[str]):
+        self.db.command('grantRolesToUser', user, roles=roles)
+
+    # database management
     def drop_database(self):
         """
         Drop current database from the MongoDB client.
         TODO: This is a dangerous operation and thus should require authentication.
         """
-        if self.database is not None:
-            self.client.drop_database(self.database)
+        if self.db is not None:
+            self.client.drop_database(self.db)
             click.secho(f'Current database dropped successfully.')
         else:
             click.secho(f'Failed: has not specified any database.', fg='red')
