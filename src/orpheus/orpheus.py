60fedff2fc548a8af4717db3863325363124cc9e
MIN-HUNG <oower454808@gmail.com>
2022-05-28 16:34:48 +0800

    metaData_info_v1

--- src/orpheus/orpheus.py
+++ src/orpheus/orpheus.py
@@ -5,35 +5,68 @@ import pandas as pd
 from typing import Dict, List
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, auc, f1_score
+from sklearn.ensemble import RandomForestClassifier
+import sklearn_json as skljson
+import sklearn
+import pymongo
+import json
+import datetime
+import os
 
-
-
-@dataclass
 class Orpheus:
-    model: DecisionTreeClassifier
-    X: pd.DataFrame
-    y: pd.Series
-    metric: str
-    best_params: dict = {}
-    history: List[dict] = []
+    def __init__(
+        self,
+        data_name: str,
+        data_x,
+        data_y,
+        model: str,
+    ):
+        self.X = data_x
+        self.Y = data_y
+        self.data_name = data_name
 
-    def fit(self):
-        '''
-        merge optuna for fitting
-        '''
+    def train(self):
+        X = self.X
+        Y = self.Y
 
-    def view_all_models(self):
-        '''
-        show history with versions
-        '''
+        # TODO X, Y to mongoDB
+        model = RandomForestClassifier(n_estimators=10, max_depth=5, random_state=0).fit(X, Y)
+        score = model.score(X, Y)
+        ct = datetime.datetime.now()
 
-    def show_diff(self):
-        '''
-        show difference between last trial and this time
-        '''
+        return model, score, ct
 
-    @staticmethod
-    def obejective(trial):
-        '''
-        optuna's fixed usage form
-        '''
+    def save_metaData_to_DB(self, model, score, ct):
+        temp_json_file = "./model.json"
+        skljson.to_json(model, temp_json_file)
+
+        if os.path.exists(temp_json_file):
+            model_dict = json.load(temp_json_file)
+            os.remove(temp_json_file)
+
+
+        model_name = type(model).__name__
+        id = 1  # not sure yet how to name model
+
+        tags = model._get_tags()
+        tags.pop('preserves_dtype', None)
+        meta_dict = {
+            "data": self.data_name,
+            "evaluation score": score,
+            "scikit_learn_version": sklearn.__version__,
+            "data_creater": "_",
+            "data_user": "_",
+            "model_creater": "_",
+            "model_user": "_",
+            "train_timestamp": ct,
+            "estimator_tags": tags,
+        }
+
+        complete_data = {
+            "training id": "{}_{}".format(model_name, id),
+            "model_dict": model_dict,
+            "meta_dict": meta_dict
+        }
+
+        #TODO save complete_data to mongodb
+
